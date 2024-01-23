a732acc50e17a8bff3afcfc6efe58049f8cd8fd1
Tomas <ignaciotomasrodriguezmillan@gmail.com>
2024-01-23 11:48:34 -0300

    Board render but not working

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
+import { useState } from "react"
 
 const TURNS = {
   X: 'x',
   O: 'o'
 }
 
-const board = Array(9).fill(null)
 
-const Square = ({ children, updateBoard, index }) => {
+const Square = ({ children, updateBoard, index, isSelected }) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`
+
   return (
-    <div className="square">
+    <div className={className}>
       {children}
     </div>
   )
 }
 
 function App() {
+
+  const [board, setBoard] = useState(Array(9).fill(null))
+  const [turn, setTurn] = useState(TURNS.X)
 
   return (
     <main className="board">
@@ -23,10 +28,19 @@ function App() {
         {
           board.map((cell, index) => {
             return (
-              <Square key={index} index={index} />
+              <Square
+                key={index}
+                index={index}
+              >
+                {board[index]}
+              </Square>
             )
           })
         }
+      </section>
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
     </main>
   )
